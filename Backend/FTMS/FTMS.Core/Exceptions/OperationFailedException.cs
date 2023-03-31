f6f58f6b25d5df17699a1c324865eedcd8de08d8
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:37:43 +0300

    Middleware ExceptionHandler

---
+++ Backend/FTMS/FTMS.Core/Exceptions/OperationFailedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTMS.Core.Exceptions
+{
+    public class OperationFailedException : Exception
+    {
+        public OperationFailedException() : base("Operation Failed")
+        {
+        }
+    }
+}
