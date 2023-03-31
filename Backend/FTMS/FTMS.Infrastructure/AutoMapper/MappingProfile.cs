2a18e340045f13dd79025bfe09b13c60b0e0170d
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:42:36 +0300

    AutoMapper

---
+++ Backend/FTMS/FTMS.Infrastructure/AutoMapper/MappingProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTMS.Infrastructure.AutoMapper
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+
+            #region User
+            //Map User (Student,Supervisor)
+
+            #endregion 
+
+            #region Company
+            //Map Company
+
+            #endregion
+             
+
+            #region Agenda
+            //Map Agenda 
+
+            #endregion
+
+
+        }
+
+    }
+}
