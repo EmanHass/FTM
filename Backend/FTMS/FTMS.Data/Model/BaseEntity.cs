7e3e50fee21aac4077282983acd911ce6242cae8
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:42:59 +0300

    Models

---
+++ Backend/FTMS/FTMS.Data/Model/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTMS.Data.Model
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+       // public string Name { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreateAt { get; set; }= DateTime.Now;   
+        public DateTime UpdateAt { get; set; }
+
+
+    }
+}
