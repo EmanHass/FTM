6e2f6b73270bc63774cf8bf351f1debd04bf9bd6
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:46:39 +0300

    SeedData and Roles User in System

---
+++ Backend/FTMS/FTMS.Data/DataLayer/DbSeeder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTMS.Data.DataLayer
+{
+    public class DbSeeder
+    {
+        //Seed data
+
+    }
+}
