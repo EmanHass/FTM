b275570ceeb8c8fe1d8010243238a1d250bff9e8
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:39:34 +0300

    Enum User Type

---
+++ Backend/FTMS/FTMS.Core/Enums/UserType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTMS.Core.Enums
+{
+    public enum UserType
+    {
+        Admin = 1,
+        Supervisor = 2,
+        Student =3
+        
+
+    }
+}
