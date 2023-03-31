30dd34c56aefea6aa65323d16263f2a8cbd68127
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:40:09 +0300

    Pagination

---
+++ Backend/FTMS/FTMS.Core/Helpers/ResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTMS.Core.Helpers
+{
+    public class Meta
+    {
+        public int page { get; set; }
+        public int pages { get; set; }
+        public int perpage { get; set; }
+        public int total { get; set; }
+        public string sort { get; set; }
+        public string field { get; set; }
+    }
+
+    public class ResponseDto
+    {
+        public Meta meta { get; set; }
+        public object data { get; set; }
+    }
+}
