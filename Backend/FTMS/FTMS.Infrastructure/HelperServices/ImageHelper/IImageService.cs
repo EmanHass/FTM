6b8a6e9388de94544350cfa9abc253f374511593
Mohammed AlAttar <altarmhmd87@gmail.com>
2023-03-31 23:45:53 +0300

    ImageService

---
+++ Backend/FTMS/FTMS.Infrastructure/HelperServices/ImageHelper/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FTMS.Infrastructure.HelperServices.ImageHelper
+{
+    public interface IImageService
+    {
+        void DeleteFile(string webRootInnerFolderPath, string fileName);
+        Task ResizeImage(string filePath, string uploadedFolder, string fileName);
+        Task<string> SaveImage(IFormFile file, string FolderName);
+    }
+}
