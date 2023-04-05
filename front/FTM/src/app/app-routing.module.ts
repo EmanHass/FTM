a9e5bf06f9cae69679e742b5889dc7b40e1391f9
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-04-06 00:04:06 +0200

    Update app module with rounting

--- front/FTM/src/app/app-routing.module.ts
+++ front/FTM/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'FTM',
+    pathMatch:'full'
+  },
+  {
+    path: 'FTM',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
