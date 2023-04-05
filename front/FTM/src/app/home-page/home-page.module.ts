1c773344476d965d6f7be2cb363bfd5ce6d3bc6a
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-04-06 00:00:19 +0200

    update homepage

--- front/FTM/src/app/home-page/home-page.module.ts
+++ front/FTM/src/app/home-page/home-page.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule
   ],
   exports:[
     HomeComponent
