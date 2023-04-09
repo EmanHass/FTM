d0698e78229ef2cb90d7e6ce6f3a99958603c30f
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-04-09 15:48:28 +0200

    create login page and update signup

--- front/FTM/src/app/auth/login/login.component.ts
+++ front/FTM/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  isStudent:boolean=true;
+  isSupervisor:boolean=false;
+  loginForm: FormGroup;
+  constructor() {
+     this.initializationFG();
+   }
 
-  constructor() { }
+   initializationFG(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        this.customEmail(),
+      ]),  
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+  customEmail(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
 
+      if (value && !value.includes('.com')) {
+        return { emailCustom: true };
+      }
+      return null;
+    };
+  }
   ngOnInit(): void {
+  }
+  showStudentForm(){
+    this.isStudent=true;
+    this.isSupervisor=false;
+
+  }
+  showSupervisorForm(){
+    this.isSupervisor=true;
+    this.isStudent=false;
+  }
+  submit(){
+    console.log(this.isStudent, this.isSupervisor);
+    if(this.isStudent){
+      //check if student valid and then navigate his to his profile 
+    }else{
+      //if supervisor then check login info. and convert it to his profile
+    }
   }
 
 }
