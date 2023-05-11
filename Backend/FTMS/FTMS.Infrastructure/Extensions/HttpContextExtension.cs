f730814a38d52a4297fbab7435f8a5d17ac5163c
Mohammed AlAttar <ALTARMHMD87@GMAIL.COM>
2023-05-11 12:47:16 +0300

    pagination

---
+++ Backend/FTMS/FTMS.Infrastructure/Extensions/HttpContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace FTMS.Infrastructure.Extensions
+{
+    public static class HttpContextExtension
+    {
+        public async static Task InsertParametersPaginationHeaders<T>(this HttpContext httpContext, IQueryable<T> queryable)
+        {
+            // select count(*) from publihser
+            double count = await queryable.CountAsync();
+            httpContext.Response.Headers.Add("totalAmountOfRecords", count.ToString());
+        }
+    }
+}
+
---
+++ Backend/FTMS/FTMS.Infrastructure/Extensions/IQueryableExtension.cs
@@ -0,0 +1,19 @@
+﻿using FTMS.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace FTMS.Infrastructure.Extensions
+{
+    public static class IQueryableExtension
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO paginationDTO)
+        {
+            return queryable.Skip((paginationDTO.Page - 1) * paginationDTO.RecordsPerPage)
+                .Take(paginationDTO.RecordsPerPage);
+        }
+    }
+}
