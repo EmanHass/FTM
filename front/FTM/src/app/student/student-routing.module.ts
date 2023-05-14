0ce8f1b5988905fd98794f58c72d0e56358f92f4
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-14 12:05:19 +0300

    Update Student page

--- front/FTM/src/app/student/student-routing.module.ts
+++ front/FTM/src/app/student/student-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentComponent } from './student/student.component';
 import { HomeComponent } from './student/home/home.component';
 import { ProfileComponent } from './student/profile/profile.component';
 import { TrainingDataComponent } from './student/training-data/training-data.component';
 import { ReportComponent } from './student/report/report.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  },
   {
     path:'home',
     component: HomeComponent
