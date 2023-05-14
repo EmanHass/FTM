dc556b9e455899c5d7bf5b9f521e3721c4b45fc0
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-14 12:08:17 +0300

    home page student

--- front/FTM/src/app/student/student/home/home.component.ts
+++ front/FTM/src/app/student/student/home/home.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  agenda:any;
+  startAgenda:any;
+  endAgenda:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.agenda=[
+      {
+        title:'بداية تسجيل الطلبة للتدريب',
+        date:'2022-06-2'
+      },
+      {
+        title:'بداية تدريب الطلبة',
+        date:'2022-06-20'
+      },
+      {
+        title:'بداية تسليم الطلبة لتقرير التدريب',
+        date:'2022-08-14'
+      },
+      {
+        title:'نهاية تسجيل الطلبة للتدريب',
+        date:'2022-06-15'
+      },
+      {
+        title:'نهاية تدريب الطلبة',
+        date:'2022-07-2'
+      },
+      {
+        title:'نهاية تسليم الطلبة لتقرير التدريب',
+        date:'2022-08-17'
+      }
+    ];
+    this.startAgenda=this.agenda.slice(0,3);
+    this.endAgenda=this.agenda.slice(3,this.agenda.length);
   }
 
 }
