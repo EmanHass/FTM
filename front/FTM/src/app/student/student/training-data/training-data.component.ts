b9993add39ac798f7af8e4694fb59b8a4f3d7e85
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-17 14:21:22 +0300

    Design for training data student page

--- front/FTM/src/app/student/student/training-data/training-data.component.ts
+++ front/FTM/src/app/student/student/training-data/training-data.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training-data.component.scss']
 })
 export class TrainingDataComponent implements OnInit {
-
+  supervisorData:any=
+    {
+      name:'يوسف أبو سلطان',
+      email:'yousef@gmail.com',
+      phone:'0592894843'
+    }
+  companyData:any=
+    {
+      name:'شركة جوال الفلسطينية',
+      field:'التصميم والبرمجة',
+      numDaysTraining:'29 يوم',
+      startTraining:'2022-06-20',
+      endTraining:'2022-08-2'
+    }
+    labelBtn:string='تعديل';
   constructor() { }
 
   ngOnInit(): void {
