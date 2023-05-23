38ff9de965a91a94a49920afbc7c6c5ebaacde53
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-23 15:22:07 +0300

    change password and phone number - student

--- front/FTM/src/app/student/student/profile/profile.component.ts
+++ front/FTM/src/app/student/student/profile/profile.component.ts
@@ -7,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   passwordLabel:string='تغيير كلمة المرور';
+  showPassModal:boolean=false;
   phoneLabel:string='تغيير رقم الهاتف';
+  showPhoneModal:boolean=false;
+  isEditPhone:boolean=false;
+  isEditPass:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  closeModal(){
+    this.showPassModal=false;
+    this.showPhoneModal=false;
+    this.isEditPass=false;
+    this.isEditPhone=false;
+  }
+  editPassword(){
+    this.showPassModal=true;
+    this.showPhoneModal=false;
+  }
+  editPhone(){
+    this.showPhoneModal=true;
+    this.showPassModal=false;
+  }
+  successEditPass(){
+    this.isEditPass=true;
+    this.isEditPhone=false;
+    setTimeout(()=>{
+      this.closeModal();
+    },2000);
+  }
+  successEditPhone(){
+    this.isEditPhone=true;
+    this.isEditPass=false;
+    setTimeout(()=>{
+      this.closeModal();
+    },2000);
+  }
 }
