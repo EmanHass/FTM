09b172ab925c99192c27355d14c3a35983b47365
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-27 23:14:29 +0300

    training company - Supervisor

--- front/FTM/src/app/supervisor/supervisor/company/company.component.ts
+++ front/FTM/src/app/supervisor/supervisor/company/company.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company.component.scss']
 })
 export class CompanyComponent implements OnInit {
-
+  addCompany:string='إضافة شركة تدريبية';
+  addCompanyExcel:string='تحميل ملف اكسل';
+  addBtn:string='إضافة';
+  editBtn:string='تعديل';
+  showModalStatus:boolean=false;
+  isEdit:boolean=false;
+  isAdd:boolean=true;
+  companies:any=[
+    {
+      name:'شركة جوال',
+      field:'التصميم والبرمجة',
+      address:'غزة',
+    },
+    {
+      name:'شركة جوال',
+      field:'شركة الإتصالات',
+      address:'رفح',
+    },
+    {
+      name:'شركة تعاون',
+      field:'التصميم والبرمجة',
+      address:'غزة',
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  showModal(){
+    this.isAdd=true;
+    this.isEdit=false;
+    this.showModalStatus=true;
+  }
+  closeModal(){
+    this.showModalStatus=false;
+  }
+  showEditModal(){
+    this.isEdit=true;
+    this.isAdd=false;
+    this.showModalStatus=true;
+  }
 }
