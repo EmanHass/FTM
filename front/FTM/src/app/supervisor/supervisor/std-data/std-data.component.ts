1d5090d5390ce0b299c927211593832d5c34f2c6
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-28 00:16:14 +0300

    student data - Supervisor

--- front/FTM/src/app/supervisor/supervisor/std-data/std-data.component.ts
+++ front/FTM/src/app/supervisor/supervisor/std-data/std-data.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./std-data.component.scss']
 })
 export class StdDataComponent implements OnInit {
-
+  students:any=[
+    {
+      name:'أسيل الوديه',
+      email:'asil@gmail.com',
+      stdNum:20180401
+    },
+    {
+      name:'إيمان حسونة',
+      email:'eman@gmail.com',
+      stdNum:20180293
+    },
+    {
+      name:'محمد العطار',
+      email:'mohammed@gmail.com',
+      stdNum:2018254
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
