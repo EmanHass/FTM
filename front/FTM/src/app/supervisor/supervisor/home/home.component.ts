d6b5fa790aab90ee9a5c70dc4d96dc7180c92279
Eman Hassouna <79720467+EmanHass@users.noreply.github.com>
2023-05-27 21:47:37 +0300

    home page - Supervisor

--- front/FTM/src/app/supervisor/supervisor/home/home.component.ts
+++ front/FTM/src/app/supervisor/supervisor/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   btnLabel:string='إضافة أجندة تدريب';
+  showModal:boolean=false;
+  add:string='إضافة';
+  edit:string='تعديل';
+  agendaForm: FormGroup;
+  isAdd:boolean=false;
+  isEdit:boolean=false;
+  previousStatus:boolean=false;
   semesters:any=[
     {
       semesterName:'الفصل الدراسي الأول',
@@ -15,21 +23,55 @@ export class HomeComponent implements OnInit {
       settings:''
     },
     {
-      semesterName:'الفصل الدراسي الأول',
+      semesterName:'الفصل الدراسي الصيفي',
       year:'2021/2022',
-      status:'نشط',
+      status:'مؤرشف',
       settings:''
     },
     {
-      semesterName:'الفصل الدراسي الأول',
+      semesterName:'الفصل الدراسي الثاني',
       year:'2021/2022',
-      status:'نشط',
+      status:'مؤرشف',
       settings:''
     }
   ];
-  constructor() { }
+  constructor() {
+    this.initializationFG();
+  }
+
+  initializationFG(): void {
+   this.agendaForm = new FormGroup({
+     semesterName: new FormControl('', [Validators.required]),  
+     semesterYear: new FormControl('', [Validators.required]),
+     startRegister: new FormControl('', [Validators.required]),
+     endRegister: new FormControl('', [Validators.required]),
+     startTraining: new FormControl('', [Validators.required]),
+     endTraining: new FormControl('', [Validators.required]),
+     startSubmitReport: new FormControl('', [Validators.required]),
+     endSubmitReport: new FormControl('', [Validators.required]),
+   });
+ }
 
   ngOnInit(): void {
   }
-
+  showAddModal(){
+    this.isAdd=true;
+    this.isEdit=false;
+    this.showModal=true;
+  }
+  showEditModal(){
+    this.isEdit=true;
+    this.isAdd=false;
+    this.showModal=true;
+  }
+  closeModal(){
+    this.showModal=false;
+    this.previousStatus=false;
+  }
+  addingAgenda(){
+    this.showModal=false;
+  }
+  showPreviousSemester(){
+    this.previousStatus=true;
+  }
 }
